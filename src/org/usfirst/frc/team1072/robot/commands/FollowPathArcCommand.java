79e490ebed6a56d7bc41ad13f9c9bf409c37b83f
Finn Frankis <20FinnF@students.harker.org>
2018-07-06 10:59:12 -0700

    Deleted unused classes and added documentation

--- src/org/usfirst/frc/team1072/robot/commands/FollowPathArcCommand.java
+++ src/org/usfirst/frc/team1072/robot/commands/FollowPathArcCommand.java
@@ -2,8 +2,16 @@ package org.usfirst.frc.team1072.robot.commands;
 
 import org.usfirst.frc.team1072.robot.RobotMap.DrivetrainConstants;
 
+/**
+ * Follows a motion profile in an arc-like manner.
+ * @author Finn Frankis
+ * @version 6/28/18
+ */
 public class FollowPathArcCommand extends FollowPathCommand
 {
+    /**
+     * Constructs a new motion profile arc.
+     */
     public FollowPathArcCommand()
     {
         super(DrivetrainConstants.ANGLE_PID);
