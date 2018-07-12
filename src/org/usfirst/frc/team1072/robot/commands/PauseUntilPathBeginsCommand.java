332d3337a0c65a63ddd93a6781979388cbe625ec
FinnitoProductions <finn@teachmy.com>
2018-07-12 14:52:42 -0700

    Modified PauseUntilPathBeginsCommand to allow for additional delay

--- src/org/usfirst/frc/team1072/robot/commands/PauseUntilPathBeginsCommand.java
+++ src/org/usfirst/frc/team1072/robot/commands/PauseUntilPathBeginsCommand.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team1072.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,14 +11,36 @@ import edu.wpi.first.wpilibj.command.Command;
 public class PauseUntilPathBeginsCommand extends Command
 {
     private FollowPathCommand fpc;
+    private double delay;
+    private double startTime;
+    
     public PauseUntilPathBeginsCommand(FollowPathCommand fpc)
     {
         this.fpc = fpc;
+        startTime = Double.MAX_VALUE;
+    }
+    
+    /**
+     * Constructs a new PauseUntilPathBeginsCommand.
+     * @param fpc the path to be followed
+     * @param delay the time in seconds to wait AFTER the path begins
+     */
+    public PauseUntilPathBeginsCommand(FollowPathCommand fpc, double delay)
+    {
+        this.fpc = fpc;
+        this.delay = delay;
+        startTime = Double.MAX_VALUE;
+    }
+    
+    public void execute()
+    {
+        if (startTime > Double.MAX_VALUE - 10 && fpc.isSetupComplete())
+            startTime = Timer.getFPGATimestamp();
     }
     
     @Override
     public boolean isFinished()
     {
-        return fpc.isSetupComplete();
+        return fpc.isSetupComplete() && Timer.getFPGATimestamp() - startTime >= delay;
     }
 }
