367218e9c0c7f38d7e9d46076a6665e7d4118721
FinnitoProductions <finn@teachmy.com>
2018-07-12 07:50:41 -0700

    Added implementation for Branch class

--- src/org/usfirst/frc/team1072/robot/commands/Branch.java
+++ src/org/usfirst/frc/team1072/robot/commands/Branch.java
@@ -11,14 +11,58 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class Branch
 {
-    ArrayList<Command> branch = new ArrayList<Command>();
+    private ArrayList<Command> branch;
+    private int currentIndex; // 
+    private boolean hasStartedCommand; // whether the command at currentIndex has started
+    private boolean didCommandRun; // whether the command at currentIndex has run at any point
+    
     public Branch()
     {
-        
+        currentIndex = 0;
+        hasStartedCommand = false;
+        didCommandRun = false;
     }
     
-    public void addCommand(Command c)
+    public Branch addCommand(Command c)
     {
-        
+        branch.add(c);
+        return this;
     }
+    
+    public int getCurrentIndex()
+    {
+        return currentIndex;
+    }
+
+    public void incrementCurrentIndex()
+    {
+        currentIndex++;
+    }
+    
+    public void setCurrentIndex (int value)
+    {
+        currentIndex = value;
+    }
+
+    public boolean isHasStartedNextCommand()
+    {
+        return hasStartedCommand;
+    }
+
+    public void setHasStartedNextCommand(boolean hasStartedNextCommand)
+    {
+        this.hasStartedCommand = hasStartedNextCommand;
+    }
+
+    public boolean isDidCommandRun()
+    {
+        return didCommandRun;
+    }
+
+    public void setCommandRun(boolean didCommandRun)
+    {
+        this.didCommandRun = didCommandRun;
+    }
+
+    
 }
