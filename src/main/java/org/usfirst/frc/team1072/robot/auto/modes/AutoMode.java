8b52bf3d11dd1bfe49445dc4a2b4dec411289b6b
Finn Frankis <finn@teachmy.com>
2018-10-06 20:46:50 -0700

    Add run() method to AutoMode

--- src/main/java/org/usfirst/frc/team1072/robot/auto/modes/AutoMode.java
+++ src/main/java/org/usfirst/frc/team1072/robot/auto/modes/AutoMode.java
@@ -3,5 +3,5 @@ package org.usfirst.frc.team1072.robot.auto.modes;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public abstract class AutoMode extends CommandGroup {
-
+	public abstract void run();
 }
